9ceb903e490ed3522e336269966bb251d5d672b2
wtftanveer <70646347+fazitanvir@users.noreply.github.com>
2022-09-06 09:12:06 +0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,7 @@
+
+--//Mini Games
+maxDice = 6						-- Maximum number of dice to roll			Default = 6
+maxDiceSides = 100				-- Maximum number of sides on dice			Default = 100
+rpsPrefix = "Showed: "			-- Rock, Paper, Scissors result prefix		Default = "Showed: "
+flipPrefix = "Flipped: "		-- Flip Coin result prefix					Default = "Flipped: "
+rollPrefix = "Rolled: "			-- Roll Dice result prefix					Default = "Rolled: "
